df35b6c526d6afcf0a45b96feaf5166310634a1d
Zachary Robinson <me@zsrobinson.com>
2022-08-28 00:33:41 -0400

    Initial commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,93 @@
 <script>
-	let title = "Lorem Ipsum";
+
+	let input = "According to a Cambridge University study, most people can read words in sentences with scrambled letters, provided the first and last letters of words are the same."
+	let output = scrambleText(input);
+
+	function rescramble() {
+		output = scrambleText(input);
+	}
+
+	function scrambleText(text) {
+		let splitText = text.split(/(?=[.\s]|\b)/);
+		let scrambledText = [];
+		for (let word of splitText) {
+
+			if (word.length <= 3) {
+				scrambledText.push(word);
+				continue;
+			}
+
+			// remove the first and last letters
+			let middleLetters = word.slice(1, -1).split("");
+			
+			// shuffle the letters
+			let shuffledMiddleLetters = shuffle(middleLetters);
+
+			// put the letters back together
+			let scrambledWord = word.slice(0, 1) + shuffledMiddleLetters + word.slice(-1);
+			scrambledText.push(scrambledWord);
+		}
+		return scrambledText.join("");
+	}
+
+	function shuffle(arr) {
+		let newArr = [];
+		while (arr.length > 0) {
+			let rand = Math.floor(Math.random() * arr.length);
+			newArr.push(arr[rand]);
+			arr.splice(rand, 1);
+		}
+		return newArr.join("");
+	}
+
 </script>
 
 <section class="section">
-    <div class="container">
+	<div class="container">
 
-      <h1 class="title">{title}</h1>
-      <h2 class="subtitle">Dolor Sit Amet</h2>
+	<h1 class="title">Word Scrambler</h1>
+	<h2 class="subtitle">Created by Zachary Robinson</h2>
 
-	  <div class="content">
-		  <p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-			eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-			Adipiscing tristique risus nec feugiat in fermentum posuere urna 
-			nec. Nec ullamcorper sit amet risus nullam eget. Vel risus commodo 
-			viverra maecenas accumsan lacus vel facilisis. Pellentesque elit 
-			eget gravida cum sociis.
-		  </p>
-	  </div>
 
-    </div>
-  </section>
+	<div class="columns">
+		<div class="column">
+			<div class="field">
+				<label class="label" for="input">Input Text</label>
+				<div class="control">
+					<textarea class="textarea" id="input" bind:value={input}></textarea>
+				</div>
+			</div>
+		</div>
+		<div class="column">
+			<div class="field">
+				<label class="label" for="output">Output Text</label>
+				<div class="control">
+					<textarea class="textarea" id="output" readonly>{output}</textarea>
+				</div>
+			</div>
+			<div class="field is-grouped">
+				<div class="control">
+					<button class="button is-link" on:click={rescramble}>
+						<span class="icon">
+							<i class="fas fa-sync-alt"></i>
+						</span>
+						<span>Rescramble</span>
+					</button>
+				</div>
+				<div class="control">
+					<button class="button" on:click={() => navigator.clipboard.writeText(output)}>
+						<span class="icon">
+							<i class="far fa-copy"></i>
+						</span>
+						<span>Copy</span>
+					</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	</div>
+</section>
 
 <style>
 </style>
